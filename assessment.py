8577ddccd55c9739d16f9abaf78a20356d3f1d2a
Bjorn Gronntun <bjorngronntun@gmail.com>
2018-09-19 23:02:18 +0200

    Made train_test_split function

---
+++ assessment.py
@@ -0,0 +1,16 @@
+import numpy as np
+from random import shuffle
+def train_test_split(X, y, test_size=0.2):
+    data_points = X.shape[0]
+    train_points = int(data_points*(1 - test_size))
+    indices = list(range(data_points))
+    shuffle(indices)
+    train_indices = indices[:train_points]
+    test_indices = indices[train_points:]
+    '''
+    print(train_indices)
+    print(test_indices)
+    print(len(train_indices))
+    print(len(test_indices))
+    '''
+    return X[train_indices, :], X[test_indices, :], y[train_indices], y[test_indices]
--- test_program.py
+++ test_program.py
@@ -2,6 +2,7 @@ import numpy as np
 from datasets import generate_test_data
 from preprocessing import polynomial_combinations
 from models import ols_fit, ridge
+from assessment import train_test_split
 
 data_points = 100
 max_degree = 5
@@ -19,3 +20,9 @@ MSE_ols = np.mean(np.sum(np.square(target - predictions_ols)))
 MSE_ridge = np.mean(np.sum(np.square(target - predictions_ridge)))
 print('MSE OLS', MSE_ols)
 print('MSE Ridge', MSE_ridge)
+
+X_train, X_test, y_train, y_test = train_test_split(X, target)
+print('X_train shape', X_train.shape)
+print('X_test shape', X_test.shape)
+print('y_train shape', y_train.shape)
+print('y_test shape', y_test.shape)
